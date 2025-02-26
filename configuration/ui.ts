19e4e198e901740dd5804b7bfd89919f756f2b28
Ogaffar <olatunde_gaffar@kenan-flagler.unc.edu>
2025-02-26 17:31:49 -0500

    Update ui.ts
    Changes to names

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[CHAT HEADER]`;
+export const CHAT_HEADER: string = `SportMachine.ai`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `AI Created by Gaffar`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const FOOTER_MESSAGE: string = `Copyright @ 2025 by Just Gaff`;
+export const CLEAR_BUTTON_TEXT: string = `Clear Chat`;
+export const PAGE_TITLE: string = `SportMachine.ai`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
