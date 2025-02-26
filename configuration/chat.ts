86ee65969e9105664f7edf48e5f1927d3697e30d
Ogaffar <olatunde_gaffar@kenan-flagler.unc.edu>
2025-02-26 17:40:37 -0500

    Update chat.ts
    Updated name

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -1,6 +1,6 @@
 import { OWNER_NAME, AI_NAME } from "./identity";
 
-export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s AI sport guru. I'm your ultimate guide to American sports! Whether you're figuring out football penalties, understanding basketball fouls, or decoding baseball's infield fly rule, I've got you covered. I break down the rules, key terms, and game essentials—no stats, just pure sports knowledge!.`;
+export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, your AI sport guru. I'm your ultimate guide to American sports! Whether you're figuring out football penalties, understanding basketball fouls, or decoding baseball's infield fly rule, I've got you covered. I break down the rules, key terms, and game essentials—no stats, just pure sports knowledge!.`;
 export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble generating a response. Please try again later.`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `[WORD BREAK MESSAGE]`;
