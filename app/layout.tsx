9add418b902dd13700293b9f6c2aeaf8a7caaaf3
Ogaffar <olatunde_gaffar@kenan-flagler.unc.edu>
2025-03-01 18:14:40 -0500

    Update layout.tsx
    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        {/* Additional metadata tags like Open Graph can go here */}
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <TooltipProvider>
