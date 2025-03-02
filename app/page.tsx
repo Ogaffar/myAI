2ec33890de52b839b23e4a7d60a73cfca3f674ec
Ogaffar <olatunde_gaffar@kenan-flagler.unc.edu>
2025-03-01 19:40:28 -0500

    Update page.tsx
    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 "use client";
-
 import ChatInput from "@/components/chat/input";
 import ChatMessages from "@/components/chat/messages";
 import ChatHeader from "@/components/chat/header";
 import useApp from "@/hooks/use-app";
-
 export default function Chat() {
   const {
     messages,
@@ -15,14 +13,13 @@ export default function Chat() {
     handleSubmit,
     clearMessages,
   } = useApp();
-
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col h-screen w-full">
       <ChatHeader clearMessages={clearMessages} />
       
-      <main className="flex-1 overflow-hidden">
-        <div className="container mx-auto h-full max-w-screen-lg flex flex-col p-4">
-          <div className="flex-1 overflow-y-auto">
+      <main className="flex-1 overflow-hidden w-full">
+        <div className="container mx-auto h-full max-w-screen-lg flex flex-col p-4 w-full">
+          <div className="flex-1 overflow-y-auto w-full">
             <ChatMessages 
               messages={messages} 
               indicatorState={indicatorState} 
@@ -30,8 +27,7 @@ export default function Chat() {
           </div>
         </div>
       </main>
-
-      <footer className="border-t">
+      <footer className="border-t w-full">
         <div className="container mx-auto max-w-screen-lg p-4">
           <ChatInput
             input={input}
